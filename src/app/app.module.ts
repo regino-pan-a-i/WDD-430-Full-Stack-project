5398b7c4adde8e244897cc475a26aa03389a846d
Andre Regino <reginopanai@gmail.com>
2025-05-10 19:47:13 -0600

    got more components in the application

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
-import { TestComponent } from './test/test.component';
+import { HeaderComponent } from './header/header.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { RecipeDetailsComponent } from './recipes/recipe-details/recipe-details.component';
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
+import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item/recipe-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ServerComponent,
-    TestComponent
-  ],
+    HeaderComponent,
+    RecipesComponent,
+    ShoppingListComponent,
+    RecipeDetailsComponent,
+    RecipeListComponent,
+    ShoppingEditComponent,
+    RecipeItemComponent,
+    ],
   imports: [
     BrowserModule
   ],
---
+++ src/app/recipes/recipe-details/recipe-details.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recipe-details',
+  standalone: false,
+  templateUrl: './recipe-details.component.html',
+  styleUrl: './recipe-details.component.css'
+})
+export class RecipeDetailsComponent {
+
+}
